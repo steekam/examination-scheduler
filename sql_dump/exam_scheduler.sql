a738aee1439197b10913595d094c1c96c27b5279
steekam <waynewanyee@gmail.com>
2018-09-18 11:17:04 +0300

    Updated sql dump

--- sql_dump/exam_scheduler.sql
+++ sql_dump/exam_scheduler.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 16, 2018 at 05:26 PM
+-- Generation Time: Sep 18, 2018 at 08:15 AM
 -- Server version: 10.2.17-MariaDB
 -- PHP Version: 7.1.21
 
@@ -25,6 +25,74 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `class_group`
+--
+
+CREATE TABLE `class_group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `course_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `faculty_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `faculty`
+--
+
+INSERT INTO `faculty` (`id`, `name`) VALUES
+(1, 'FIT'),
+(3, 'STH'),
+(4, 'SIMS'),
+(5, 'SOA'),
+(6, 'SHSS'),
+(7, 'SMC'),
+(8, 'SLS'),
+(9, 'SGS-Masters');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty_rep`
+--
+
+CREATE TABLE `faculty_rep` (
+  `rep_id` int(11) NOT NULL,
+  `faculty_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `faculty_rep`
+--
+
+INSERT INTO `faculty_rep` (`rep_id`, `faculty_id`) VALUES
+(20, 1),
+(21, 8);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `password_reset`
 --
 
@@ -34,6 +102,20 @@ CREATE TABLE `password_reset` (
   `reset_code` varchar(255) NOT NULL,
   `sent_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `valid` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit`
+--
+
+CREATE TABLE `unit` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `unit_code` varchar(255) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `class_group` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -59,11 +141,40 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `role`, `registered_at`, `activated`) VALUES
-(13, 'SWanyee', 'waynewanyee@gmail.com', '$2y$10$aRsG7wpHpxr7H5XJRVLZ8OqOcdBrwrZc.z1wdhMbYMQje6.xX/wai', 'Stephen', 'Wanyee', 'administrator', '2018-09-11 19:31:42', 'true');
+(13, 'SWanyee', 'waynewanyee@gmail.com', '$2y$10$aRsG7wpHpxr7H5XJRVLZ8OqOcdBrwrZc.z1wdhMbYMQje6.xX/wai', 'Stephen', 'Wanyee', 'administrator', '2018-09-11 19:31:42', 'true'),
+(20, 'HMundui', 'hellen@gmail.com', '$2y$10$TvmntuNdn20fe/lIHnJ/GOVyAITOAlJ49DKkfM9sx2nOEoOYYTEeS', 'Hellen', 'Mundui', 'faculty representative', '2018-09-16 23:36:13', 'false'),
+(21, 'MJordan', 'michael.jords@mail.com', '$2y$10$zowI5sFKlIJbEbL0mDPLVOwjpGpT6gF8wMeExO.4xKxSon2NM8Q1u', 'Michael', 'Jordan', 'faculty representative', '2018-09-16 23:36:57', 'false');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `class_group`
+--
+ALTER TABLE `class_group`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `faculty_id` (`faculty_id`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `faculty_rep`
+--
+ALTER TABLE `faculty_rep`
+  ADD KEY `rep_id` (`rep_id`),
+  ADD KEY `faculty_id` (`faculty_id`);
 
 --
 -- Indexes for table `password_reset`
@@ -71,6 +182,14 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `first_name`, `last_
 ALTER TABLE `password_reset`
   ADD PRIMARY KEY (`id`),
   ADD KEY `password_reset_ibfk_1` (`user_id`);
+
+--
+-- Indexes for table `unit`
+--
+ALTER TABLE `unit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id` (`course_id`),
+  ADD KEY `class_group` (`class_group`);
 
 --
 -- Indexes for table `users`
@@ -83,26 +202,76 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `class_group`
+--
+ALTER TABLE `class_group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `password_reset`
 --
 ALTER TABLE `password_reset`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
+-- AUTO_INCREMENT for table `unit`
+--
+ALTER TABLE `unit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `class_group`
+--
+ALTER TABLE `class_group`
+  ADD CONSTRAINT `class_group_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`);
+
+--
+-- Constraints for table `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY (`faculty_id`) REFERENCES `faculty` (`id`);
+
+--
+-- Constraints for table `faculty_rep`
+--
+ALTER TABLE `faculty_rep`
+  ADD CONSTRAINT `faculty_rep_ibfk_1` FOREIGN KEY (`rep_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `faculty_rep_ibfk_2` FOREIGN KEY (`faculty_id`) REFERENCES `faculty` (`id`);
+
+--
 -- Constraints for table `password_reset`
 --
 ALTER TABLE `password_reset`
   ADD CONSTRAINT `password_reset_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `unit`
+--
+ALTER TABLE `unit`
+  ADD CONSTRAINT `unit_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`),
+  ADD CONSTRAINT `unit_ibfk_2` FOREIGN KEY (`class_group`) REFERENCES `class_group` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
